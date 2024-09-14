7385de6e87515d71151154ad785220ca2c9ed5e6
Maryam Nasseri <46525096+Maryam-Nasseri@users.noreply.github.com>
2024-09-14 19:31:50 +0100

    Update training_args.py

--- training_args.py
+++ training_args.py
@@ -2,6 +2,6 @@
 training_args = TrainingArguments(
     output_dir='final',               # name of output folder/directory
     overwrite_output_dir = True,      # set to True if you want to incrementally train and test the model
-    evaluation_strategy="epoch",
-    num_train_epochs=1,
+    evaluation_strategy="epoch",      # evaluation in steps or epochs
+    num_train_epochs=1,               # how many times to go through the whole training dataset 
 )
