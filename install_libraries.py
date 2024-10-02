c41372e9515b5628f7a999e5a1132908a306f8c5
Maryam Nasseri <46525096+Maryam-Nasseri@users.noreply.github.com>
2024-10-02 18:29:57 +0100

    Update install_libraries.py

--- install_libraries.py
+++ install_libraries.py
@@ -1,9 +1,11 @@
 # Important libraries to work with for fine-tuning with LORA and QLoRA methods
 # use pip install as needed
 
-pip install transformers
+pip install transformers   #transformers library for all models and configurations
 pip install peft  #for the peft model and peft configuration
 pip install trl
 pip install einops  #for working with tensors
-pip install keras
+
+
+pip install keras   # additional functionalities for processing data
 
