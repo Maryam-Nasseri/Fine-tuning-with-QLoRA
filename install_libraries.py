e3b450abac35dc63983b1e94c152ea5518b3db78
Maryam Nasseri <46525096+Maryam-Nasseri@users.noreply.github.com>
2024-10-03 14:35:32 +0100

    Update install_libraries.py

--- install_libraries.py
+++ install_libraries.py
@@ -4,8 +4,11 @@
 pip install transformers   #transformers library for all models and configurations
 pip install peft  #for the peft model and peft configuration
 pip install trl
+
+#Working with tensors
+
 pip install einops  #for working with tensors
 
-
-pip install keras   # additional functionalities for processing data
-
+# additional functionalities for processing data
+pip install keras  
+pip install pandas
