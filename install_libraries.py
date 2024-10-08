d6ccdb6fa1ee1dc909886cf60e8eb9046d691311
Maryam Nasseri <46525096+Maryam-Nasseri@users.noreply.github.com>
2024-10-08 22:38:48 +0100

    Update install_libraries.py

--- install_libraries.py
+++ install_libraries.py
@@ -5,14 +5,14 @@ pip install transformers==4.38.2   #transformers library for all models and conf
 pip install peft  #for the peft model and peft configuration
 pip install trl
 pip install accelerate==0.27.2
+pip install pandas  #processing data
 
-#Working with tensors
 pip install torch   #Pytorch for deep learning
 pip install einops  #for working with tensors
 
-# Additional functionalities for processing data; training/evaluation datasets; training evaluation
+# Additional useful libraries for the fine-tuning process          
 pip install datasets
-pip install keras  
-pip install pandas
+pip install keras  #training/evaluation datasets
+
 pip install numpy
-pip install evaluate
+pip install evaluate   #training evaluation
