7dfaa2084374559c82e0d463c21544b1fd471a8b
Maryam Nasseri <46525096+Maryam-Nasseri@users.noreply.github.com>
2024-10-14 18:10:52 +0100

    Update install_libraries.py

--- install_libraries.py
+++ install_libraries.py
@@ -6,8 +6,7 @@ pip install peft==0.4.0  #for the peft model and peft configuration
 pip install trl==0.4.7
 pip install accelerate==0.27.2
 pip install pandas==2.1.4  #processing data
-pip install torch   #Pytorch for deep learning
-
+pip install torch==2.2.1   #Pytorch for deep learning
 
 
 # Additional useful libraries for the fine-tuning process          
@@ -15,4 +14,7 @@ pip install datasets==2.20.0
 pip install keras==2.16.0  #training/evaluation datasets
 pip install einops  #for working with tensors
 pip install numpy==1.26.4
-pip install evaluate   #training evaluation
+pip install evaluate==0.4.2   #training evaluation
+
+# for pulling models and pushing fine-tuned models
+pip install --upgrade huggingface_hub==0.23.4
